77232d9c00050f9bf04952118c6b062d032c9087
huyluvnhjm99 <34624305+huyluvnhjm99@users.noreply.github.com>
2020-06-26 12:41:22 +0700

    dsad

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace pto_restful_service
             config.MapHttpAttributeRoutes();
             config.Routes.MapHttpRoute(
                name: "DefaultApi",
-               routeTemplate: "api/v1/{controller}/{id}",
+               routeTemplate: "api/{controller}/{id}",
                defaults: new { id = RouteParameter.Optional}
            );
 
